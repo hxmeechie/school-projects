9f256023dda17b9a5aabcf2226981f06db282668
hxmeechie <112818265+hxmeechie@users.noreply.github.com>
2024-11-25 10:52:09 +0100

    :sparkles: Added new project - RGB Mixer

---
+++ rgb-mixer/RGBMixer/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace RGBMixer
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
