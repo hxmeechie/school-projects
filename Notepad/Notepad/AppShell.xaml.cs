5edcf8717704825da9de3d320c34bd7b10ddfee3
Tomasz Chada <egzamin@poczta.pl>
2026-05-11 10:30:41 +0200

    ✨ Added new project - .NET MAUI Notepad App

---
+++ Notepad/Notepad/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Notepad
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Notepad/Notepad/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿namespace Notepad
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnEditorTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string currentText = e.NewTextValue ?? string.Empty;
+
+            int characterCount = currentText.Length;
+            CharCountLabel.Text = characterCount.ToString();
+
+            string[] words = currentText.Split([' ', '\n'], StringSplitOptions.RemoveEmptyEntries);
+            int wordCount = words.Length;
+            WordCountLabel.Text = wordCount.ToString();
+        }
+    }
+}
